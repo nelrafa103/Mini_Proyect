71bc04ea260f27e8bf9f7af1d093ac6117d526a1
Nelcido Diaz <nelcidodiaz13@gmail.com>
2020-07-23 20:09:58 -0400

    New Files and Big's changes
    I changed the structure of the proyect to /n
    a modular structure, i decided to make a only class for all the chips /n
    called ChipsOfGame (You can find it in file chess_chips.py) /n
    I deleted the mayority of the fuction to move the chips /n
    by: Nelcido
    date: 23/Jul/2020
    files: chess_board.py and chess_chips.py(added)

--- chess_board.py
+++ chess_board.py
@@ -1,45 +1,39 @@
 import os
-import turtle
 
 
 class Message():
-        def play(self):
-            entry_1 = input("Enter the position of the chip you want to use")
+    def play(self):
+        self.entry_1 = input("Enter the position of the chip you want to use")
 
-        def next_position(self):
-            entry_2 = input("Enter the next position for you chip")
+    def next_position(self):
+        self.entry_2 = input("Enter the next position for you chip")
+        self.entry_3 = input()
+
+
 messages = Message()
 
+
 class Properies(object):
-            def __init__(self, position_x, position_y, color):
-                self.position_x = position_x
-                self.position_y = position_y
-                self.color = color
+    def __init__(self, position_x, position_y, color):
+        self.position_x = position_x
+        self.position_y = position_y
+        self.color = color
 
-            def remove_from_board(self):
-                self.position_x = None
-                self.position_y = None
+    def walk_horizontal(self, value=None):
+        self.value = value
+        self.position_x = self.value[0]
 
-            def walk_front(self, value):
-                self.value = value
-                self.position_y += self.value
+    def walk_vertical(self, value=None):
+        self.value = value
+        self.position_y = self.value[1]
 
-            def walk_back(self, value):
-                self.value = value
-                self.position_y -= self.value
+    def remove_from_board(self):
+        self.position_x = None
+        self.position_y = None
 
-            def walk_left(self, value):
-                self.value = value
-                self.position_x -= self.value
-
-            def walk_right(self, value):
-                self.value = value
-                self.position_x += self.value
-
-            def eat_in_chess(self, eaten, attaker):
-                self.eaten = eaten
-                self.attaker = attaker
-                if self.attaker.position_x == self.eaten.position_x:
-                    eaten.remove_from_board()
-
-print('It is not a correct value,debbung again.Please!')
+    def eat_in_chess(self, eaten, attaker):
+        self.eaten = eaten
+        self.attaker = attaker
+        if self.attaker.position_x == self.eaten.position_x and self.attaker.position_y == self.eaten.position_y:
+            eaten.remove_from_board()
+# messages.next_position()
--- chess_chips.py
+++ chess_chips.py
@@ -1,93 +1,42 @@
-import chess_board
-class Pawn(chess_board.Properies):
-    @property
-    def movements_pawn(self):
-        self.walk_front(1)
+from chess_board import Properies
+from chess_board import messages
+# This class contain all the movemets in the chips and attack
 
-    def attack_pawn(self, eaten, attaker):
+# messages.next_position()
+# messages.next_position()
+LETTERS_IN_BOARD = {'A': 1, 'B': 2, 'C': 3,
+                    'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8}
+VALUES_OF_THE_CHIPS = {'Black': {'Pawn_1': [2, 'A'], 'Pawn_2': [2, 'B'], 'Pawn_3': [2, 'C'], 'Pawn_4': [2, 'D'], 'Pawn_5': [2, 'E'], 'Pawn_6': [2, 'F'], 'Pawn_7': [2, 'G'], 'Pawn_8': [2, 'H']},
+  
+                       'White': {'Pawn_1': [7, 'A'], 'Pawn_2': [7, 'B'], 'Pawn_3': [7, 'C'], 'Pawn_4': [7, 'D'], 'Pawn_5': [7, 'E'], 'Pawn_6': [7, 'F'], 'Pawn_7': [7, 'G'], 'Pawn_8': [7, 'H']}}
+
+       
+
+class ChipsOfGame(Properies):
+
+    def attack(self, eaten, attaker):
         self.eat_in_chess(eaten, attaker)
 
+    def movement_front(self, value):
+        self.value = value
+        self.walk_horizontal(self.value)
 
-class Dame(chess_board.Properies):
-    @property
-    def movement_1(self):
-        self.walk_front(messages.entry_2)
+    def movement_right(self, value):
+        self.value = value
+        self.walk_vertical(self.value)
 
-    def movement_2(self):
-        self.walk_back(messages.entry_2)
-
-    def movement_3(self):
-        self.walk_left(messages.entry_2)
-
-    def movement_4(self):
-        self.walk_right(messages.entry_2)
-
-    def movement_5(self):
-        self.walk_right(messages.entry_2)
-        self.walk_front(messages.entry_2)
-
-    def movement_6(self):
-        self.walk_left(messages.entry_2)
-        self.walk_front(messages.entry_2)
-
-    def attack_dame(self, eaten, attaker):
-        self.eat_in_chess(eaten, attaker)
+    def movement_inline_right(self, value):
+        self.value = value
+        self.walk_horizontal(self.value)
+        self.walk_vertical(self.value)
 
 
-class Bishop(chess_board.Properies):
-    @property
-    def movement_1(self):
-        self.walk_right(messages.entry_2)
-        self.walk_front(messages.entry_2)
-
-    def movement_2(self):
-        self.walk_left(messages.entry_2)
-        self.walk_front(messages.entry_2)
-
-    def attack_bishop(self, eaten, attaker):
-        self.eat_in_chess(eaten, attaker)
-
-
-class Tower(chess_board.Properies):
-    @property
-    def movement_1(self):
-        self.walk_front(messages.entry_2)
-
-    def movement_2(self):
-        self.walk_back(messages.entry_2)
-
-    def movement_3(self):
-        self.walk_left(messages.entry_2)
-
-    def movement_4(self):
-        self.walk_right(messages.entry_2)
-
-    def attack_tower(self, eaten, attaker):
-        self.eat_in_chess(eaten, attaker)
-
-
-class King(chess_board.Properies):
-    @property
-    def movement_1(self):
-        self.walk_front(1)
-
-    def movement_2(self):
-        self.walk_back(1)
-
-    def movement_3(self):
-        self.walk_left(1)
-
-    def movement_4(self):
-        self.walk_right(1)
-
-    def movement_5(self):
-        self.walk_right(1)
-        self.walk_front(1)
-
-    def movement_6(self):
-        self.walk_left(1)
-        self.walk_front(1)
-
-    def attack_king(self, eaten, attaker):
-        self.eat_in_chess(eaten, attaker)
-
+# VALUES_OF_THE_CHIPS = {'Black':Firts2.movement_front}
+# print(VALUES_OF_THE_CHIPS['Black'])
+Rules_ = ChipsOfGame(14, VALUES_OF_THE_CHIPS['Black']['Pawn_1'][1], 'White')
+print(Rules_.position_x)
+messages.next_position()
+Rules_.movement_front(messages.entry_2)
+# Rules_.movement_inline_right(messages.entry_2)
+#Rules_.movement_front(Rules_.i + 1)
+print(Rules_.position_y, Rules_.position_x)
